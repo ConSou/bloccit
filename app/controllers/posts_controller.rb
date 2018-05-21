a109c993a8d877f8e911cd981644bed765a59c43
Conor Souhrada <ConorSouhrada@gmail.com>
2018-05-21 14:06:13 -0600

    assignment-17 commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def index
 
     @posts = Post.all
+    @posts.each do |post|
+      if post.id % 5 == 0 || post.id == 1
+        post.title = 'SPAM'
+      end
+    end
   end
 
   def show
