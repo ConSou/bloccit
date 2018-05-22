9944b54ffa8436c5281a8e47b7ac4ded285d8bb8
Conor Souhrada <ConorSouhrada@gmail.com>
2018-05-22 16:18:14 -0600

    assignment-16 revisions

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ require 'random_data'
  end
  comments = Comment.all
 
-posts.find_or_create_by!(title: "Unique Title", body: "A unique body for a unique post.")
-comments.find_or_create_by!(post: Post.find(51), body: "A unique comment!")
+unique_post = posts.find_or_create_by!(title: "Unique Title", body: "A unique body for a unique post.")
+comments.find_or_create_by!(post: unique_post, body: "A unique comment!")
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
