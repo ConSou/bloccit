6d45466ee28be4ba123918d7c5a6a61b2fc0b684
Conor Souhrada <ConorSouhrada@gmail.com>
2018-05-29 17:48:13 -0600

    checkpoint-21 commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,4 @@
 class PostsController < ApplicationController
-  def index
-
-    @posts = Post.all
-  end
 
   def show
 
@@ -10,7 +6,7 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -19,12 +15,15 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+
+    @post.topic = @topic
 
     if @post.save
       flash[:notice] = "Post was saved!"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
-      flash.new[:alert] = "There was an error saving the post.  Please try again."
+      flash.now[:alert] = "There was an error saving the post.  Please try again."
       render :new
     end
   end
@@ -41,7 +40,7 @@ class PostsController < ApplicationController
 
      if @post.save
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -53,7 +52,7 @@ class PostsController < ApplicationController
 
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was successfully deleted"
-       redirect_to posts_path
+       redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post, please try again."
        render :show
