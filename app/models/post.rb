d15aec46e3e9f82d308f6ce2b2792597f33b7def
Conor Souhrada <ConorSouhrada@gmail.com>
2018-06-06 17:48:01 -0600

    assignment-26 commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   default_scope { order ("created_at DESC") }
+  scope :ordered_by_title, -> {order("title DECS")}
+  scope :ordered_by_reverse_created_at, -> {order("created_at default_scope")}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
