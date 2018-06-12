71ffdbd265af1132a3933f50d01a181df7da02d6
Conor Souhrada <ConorSouhrada@gmail.com>
2018-06-11 19:05:49 -0600

    assignment-30 commit

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -10,6 +10,18 @@ class FavoriteMailer < ApplicationMailer
     @post = post
     @comment = comment
 
-    mail(to: user.email, subject: "New Comment on #{post.title}")  
+    mail(to: user.email, subject: "New Comment on #{post.title}")
   end
+
+  def new_post(user, post)
+    headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, subject: "You have created #{post.title}")
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,8 @@ class Post < ApplicationRecord
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :favorite_post_auto
+
   def up_votes
 
     votes.where(value: 1).count
@@ -32,6 +34,13 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+private
+
+  def favorite_post_auto
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self.user, self).deliver_now
   end
 
 end
