30d201fae68b5b57ac6da0554335c7e5ac111ceb
Conor Souhrada <ConorSouhrada@gmail.com>
2018-06-19 10:33:51 -0600

    helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,17 @@
 module UsersHelper
-  def no_posts?
-    @user.posts.count == 0
+  def show_posts_if_any(user)
+    if user.posts.count == 0
+      "<p>#{user.name} has not submitted any posts</p>".html_safe
+    else
+       render user.posts
+    end
+  end
+
+  def show_comments_if_any(user)
+    if user.comments.count == 0
+      "<p>#{user.name} has not submitted any comments</p>".html_safe
+    else
+      render user.comments
+    end
   end
 end
